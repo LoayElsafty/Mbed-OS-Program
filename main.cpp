058d278015bbf7a35f29436b7a16b9a3771bf16b
loay_hatem <loayhatem4@gmail.com>
2025-01-31 10:45:34 +0000

    Test2

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
 #include "mbed.h"
+#include <chrono>
+#include <stdio.h>
+
+#define Buff_Lenth 64
+
+int length;
+char buffer[Buff_Lenth] = {0};
+BufferedSerial pc(USBTX, USBRX, 115200);
 
 // main() runs in its own thread in the OS
 int main()
 {
     while (true) {
+        length = snprintf(buffer, Buff_Lenth, "\r\nHello");
+        pc.write(buffer, length);
+        ThisThread::sleep_for(chrono::seconds(1));
 
     }
 }
