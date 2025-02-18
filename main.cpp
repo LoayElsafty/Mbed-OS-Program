b0ebda61d4391446f64146c8edc18f3a87200d2c
LoayElsafty <loayhatem4@gmail.com>
2025-02-18 17:35:19 +0000

    updated

--- main.cpp
+++ main.cpp
@@ -1,44 +1,40 @@
 #include "mbed.h"
 #include <chrono>
-#include <cstdint>
-#include <cstdio>
-#include <cstring>
 #include <stdio.h>
+#define Buff_Length 512
 
-#define BLINKING_RATE 10ms
-#define MSG_BUFF_LENGTH 32
+
+char buffer[Buff_Length] = {0};
 BufferedSerial pc(USBTX, USBRX, 115200);
-char MSG[32];
-int length;
-char buff[32] = {0};
-DigitalOut led(LED1);
-DigitalOut led1(D2);
+int heart_rate = 0;
 
 // main() runs in its own thread in the OS
 int main()
 {
-    length = snprintf(MSG, MSG_BUFF_LENGTH,"\r\nProgramme Starting\r\n");
-    pc.write(MSG,length);
-
+    int counter = 0;
     while (true) {
-        led = !led;
-        if (uint32_t num = pc.read(buff, sizeof(buff)))
-        {
-            if (strcmp(buff, "1") == 0 )
-            {
-                led1 = !led1;
-                length = snprintf(MSG, MSG_BUFF_LENGTH, "\r\nReceived Command LED1\r\n");
-                pc.write(MSG,length);
-            }
-            else if (strcmp(buff, "s") == 0 )
-            {
-                length = snprintf(MSG, MSG_BUFF_LENGTH, "\r\n{Status :{LED1:%d}}\r\n", led1.read());
-                pc.write(MSG,length);
-            }
-        }
-        ThisThread::sleep_for(BLINKING_RATE);
-        
+
+// Generate dynamic patient data
+        int heart_rate = 60 + (rand() % 41); // Random heart rate between 60-100
+        int systolic_bp = 90 + (rand() % 31); // Random systolic blood pressure between 90-120
+        int diastolic_bp = 60 + (rand() % 21); // Random diastolic blood pressure between 60-80
+        int body_temperature = 30 + static_cast<int>(rand() % 15) / 10.0f; // Random temp between 36.0-37.4°C
+
+        // Generate JSON message
+        int length = snprintf(buffer, Buff_Length,
+                              "{\r\n"
+                              "  \"Data\": [\r\n"
+                              "    {\"Heart Rate\": %d},\r\n"
+                              "    {\"Blood Pressure\": (\"Systolic\": %d, \"Diastolic\": %d)},\r\n"
+                              "    {\"Body Temperature\": %d}\r\n"
+                              "  ]\r\n"
+                              "}\r\n",
+                              heart_rate, systolic_bp, diastolic_bp, body_temperature);
+        pc.write(buffer, length);
+
+        counter++;
+
+        ThisThread::sleep_for(chrono::seconds(1));
 
     }
 }
-
